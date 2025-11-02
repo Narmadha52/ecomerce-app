b8b3f10815240ed51237dc6d83eeaef5101a88c4
Narmadha52 <gunasekarnarmadha@gmail.com>
2025-11-02 21:40:14 +0530

    Delete backend/src/main/java/com/ecommerce/backend/payment directory

--- backend/src/main/java/com/ecommerce/backend/payment/repository/TransactionRepository.java
+++
@@ -1,9 +0,0 @@
-// src/main/java/com/ecommerce/backend/payment/repository/TransactionRepository.java
-
-package com.ecommerce.backend.payment.repository;
-
-import com.ecommerce.backend.payment.model.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-}
