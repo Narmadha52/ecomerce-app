d0c2dba71bcc6a03820233156d5841566b6b8a03
Narmadha52 <gunasekarnarmadha@gmail.com>
2025-11-02 21:39:37 +0530

    Delete backend/src/main/java/com/ecommerce/backend/category directory

--- backend/src/main/java/com/ecommerce/backend/category/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-// src/main/java/com/ecommerce/backend/category/repository/CategoryRepository.java
-
-package com.ecommerce.backend.category.repository;
-
-import com.ecommerce.backend.category.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
