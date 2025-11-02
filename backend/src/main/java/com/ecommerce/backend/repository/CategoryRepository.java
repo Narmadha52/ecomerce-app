10311edb90db6ff5f060a30eb7e1783e2dc01ea1
Narmadha52 <gunasekarnarmadha@gmail.com>
2025-11-02 21:43:02 +0530

    Update CategoryRepository.java

--- backend/src/main/java/com/ecommerce/backend/repository/CategoryRepository.java
+++ backend/src/main/java/com/ecommerce/backend/repository/CategoryRepository.java
@@ -2,7 +2,7 @@
 
 package com.ecommerce.backend.repository;
 
-import com.ecommerce.backend.category.model.Category;
+import com.ecommerce.backend.category.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
