e11037cf235da7e0abe71fcd523c6f240f9ef33d
Narmadha52 <gunasekarnarmadha@gmail.com>
2025-11-02 22:02:56 +0530

    Update TransactionRepository.java

--- backend/src/main/java/com/ecommerce/backend/repository/TransactionRepository.java
+++ backend/src/main/java/com/ecommerce/backend/repository/TransactionRepository.java
@@ -1,8 +1,8 @@
 // src/main/java/com/ecommerce/backend/payment/repository/TransactionRepository.java
 
-package com.ecommerce.backend.payment.repository;
+package com.ecommerce.backend.repository;
 
-import com.ecommerce.backend.payment.model.Transaction;
+import com.ecommerce.backend.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
