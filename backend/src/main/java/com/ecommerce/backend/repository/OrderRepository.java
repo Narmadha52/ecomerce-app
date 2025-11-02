5bd9867458881a62e259e3a3534fb5402b29e952
Narmadha52 <gunasekarnarmadha@gmail.com>
2025-11-02 22:02:14 +0530

    Update OrderRepository.java

--- backend/src/main/java/com/ecommerce/backend/repository/OrderRepository.java
+++ backend/src/main/java/com/ecommerce/backend/repository/OrderRepository.java
@@ -1,6 +1,6 @@
-package com.ecommerce.backend.order.repository;
+package com.ecommerce.backend.repository;
 
-import com.ecommerce.backend.order.model.Order;
+import com.ecommerce.backend.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
