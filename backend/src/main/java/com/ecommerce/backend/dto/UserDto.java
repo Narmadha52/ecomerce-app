8f906feaffdfd7cea24939670c581bdd58779b69
Narmadha52 <gunasekarnarmadha@gmail.com>
2025-11-02 21:53:14 +0530

    Add files via upload

---
+++ backend/src/main/java/com/ecommerce/backend/dto/UserDto.java
@@ -0,0 +1,25 @@
+package com.ecommerce.backend.user.dto;
+
+import com.ecommerce.backend.user.model.Role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+/**
+ * Data Transfer Object for transferring User data.
+ * Used primarily for exposing user details (excluding the password) to the client.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+    private Long id;
+    private String username;
+    private String email;
+    private String firstName;
+    private String lastName;
+    // The previous compilation errors were caused by missing the import for this class:
+    private Set<Role> roles;
+}
